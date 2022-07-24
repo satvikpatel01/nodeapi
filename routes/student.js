b962975601d1be1cfcc671b3adebffebd891e8a1
satvik <satvikribadiya1997@gmail.com>
2022-07-24 12:51:43 +0530

    fix: student controller

--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const studentController = require("../controllers/StudentController");
 
 router.get("/get", (req, res) => {
+
   return studentController.student.get(req, res);
 });
 router.get("/get-student-by-id", (req, res) => {
